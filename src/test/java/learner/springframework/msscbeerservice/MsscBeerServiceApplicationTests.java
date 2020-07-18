5a2731376e794b9e3d4feb32db443aeeea09790d
sadia_sher_360factors <sadia.sher@360factors.com>
2020-07-18 17:51:03 +0500

    Initial project add commit

---
+++ src/test/java/learner/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package learner.springframework.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
